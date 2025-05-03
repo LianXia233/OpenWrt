dc851bc110b6bc77be775ac497cb741fdf612d08
LianXia233 <49828512+LianXia233@users.noreply.github.com>
2025-05-04 04:13:02 +0800

    Update Feeds.sh

--- Scripts/LEDE/HinLink_H29K/Feeds.sh
+++ Scripts/LEDE/HinLink_H29K/Feeds.sh
@@ -10,10 +10,6 @@
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git modem https://github.com/FUjr/modem_feeds.git;main' >> feeds.conf.default
+echo 'src-git modem https://github.com/FUjr/modem_feeds.git;main' >>feeds.conf.default
+echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy.git;main' >>feeds.conf.default
